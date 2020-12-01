d4e37e90734983678b0b8b34aeb1c78536cdb0d2
Michael Hall <mlh0079@uah.edu>
2020-12-01 13:47:33 -0600

    - Changed tests to use unit testing style assertions.

--- tests/test_fetsh_granules.py
+++ tests/test_fetsh_granules.py
@@ -6,14 +6,15 @@ import unittest
 class TestDiscoverGranules(unittest.TestCase):
     def test_get_file_link(self):
         tempList = DiscoverGranules.get_files_link_http('http://data.remss.com/ssmi/f16/bmaps_v07/y2020/m04/')
-        assert len(tempList) == 61
+        self.assertEqual(len(tempList), 61)
 
     def test_get_file_link_wregex(self):
         tempList = DiscoverGranules.get_files_link_http('http://data.remss.com/ssmi/f16/bmaps_v07/y2020/m04/', "\Bd3d")
-        assert len(tempList) == 30
+        self.assertEqual(len(tempList), 30)
 
     def test_bad_url(self):
-        DiscoverGranules.get_files_link_http('malformed url')
+        tempList = DiscoverGranules.get_files_link_http('malformed url')
+        self.assertEqual(len(tempList), 0)
 
 
 if __name__ == "__main__":
