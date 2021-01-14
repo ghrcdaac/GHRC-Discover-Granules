b9a426bd314c39a9206d3185e3ef4c4ff8a9dcbd
Michael Hall <mlh0079@uah.edu>
2021-01-14 09:43:25 -0600

    - Current working state.

---
+++ lambda.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+   region = "us-east-1"
+   profile = "default"
+}
+
+resource "aws_lambda_function" "discover_granules" {
+   function_name = "discover_granules"
+
+   # "main" is the filename within the zip file (main.js) and "handler"
+   # is the name of the property under which the handler function was
+   # exported in that file.
+   handler = "lambda_function.lambda_handler"
+   runtime = "python3.8"
+   filename = "./task/dist/package.zip"
+
+   role = aws_iam_role.lambda_exec.arn
+}
+
+ # IAM role which dictates what other AWS services the Lambda function
+ # may access.
+resource "aws_iam_role" "lambda_exec" {
+   name = "test_example"
+
+   assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
