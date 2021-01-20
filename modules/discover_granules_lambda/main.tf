3a94cd8c7ffb1181a7b6d6f5036c8f0db9bc2c80
Michael Hall <mlh0079@uah.edu>
2021-01-20 10:42:51 -0600

    - Implemented functionality for retrieve Lambda ARN using terraform output command.

---
+++ modules/discover_granules_lambda/main.tf
@@ -0,0 +1,16 @@
+resource "aws_lambda_function" "discover_granules" {
+   function_name = "discover_granules"
+   handler = "lambda_function.lambda_handler"
+   runtime = "python3.8"
+   filename = "./task/dist/package.zip"
+   role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_iam_role" "lambda_exec" {
+   name = "aws_iam_lambda"
+   assume_role_policy = file("policy.json")
+}
+
+output "arn" {
+  value = aws_lambda_function.discover_granules.arn
+}
