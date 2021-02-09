089bca332727769d061334e6c5b2ab90a7ac80ca
Michael Hall <mlh0079@uah.edu>
2021-02-09 13:42:59 -0600

    - Updated for NGAP.

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "aws_profile" {
+  type    = string
+  default = "default"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "prefix" {
+  type = string
+  default = null
+}
+
+variable "cumulus_lambda_role" {
+  type = string
+  default = null
+}
+
+variable "lambda_subnet_ids" {
+  type = list(string)
+  default = null
+}
+
+variable "lambda_security_group_ids" {
+  type = list(string)
+  default = null
+}
