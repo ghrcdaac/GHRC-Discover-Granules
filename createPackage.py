183fcabe26ef4731fa9a8b7f2650c006b4ad3cd9
amarouane <amarouane@itsc.uah.edu>
2021-03-10 09:56:02 -0600

    c=allow local development

--- createPackage.py
+++ createPackage.py
@@ -4,5 +4,5 @@ import pip
 
 pip.main(['install', '--target', './package', '-r', 'requirements.txt'])
 [shutil.copy(ele, './package/') for ele in ['./task/granule.py', './task/lambda_function.py', './task/main.py']]
-shutil.make_archive('./task/dist/package', 'zip', './package')
+shutil.make_archive('./package', 'zip', './package')
 shutil.rmtree('./package')
