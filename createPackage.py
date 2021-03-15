e4c2b94568d7cf2665a2e4f0fa665b96f22e3197
amarouane <amarouane@itsc.uah.edu>
2021-03-15 11:52:25 -0500

    Replace Granule class with a minimalistic implementation

--- createPackage.py
+++ createPackage.py
@@ -3,6 +3,6 @@ import shutil
 import pip
 
 pip.main(['install', '--target', './package', '-r', 'requirements.txt'])
-[shutil.copy(ele, './package/') for ele in ['./task/granule.py', './task/lambda_function.py', './task/main.py']]
+[shutil.copy(ele, './package/') for ele in ['./task/lambda_function.py', './task/main.py']]
 shutil.make_archive('./package', 'zip', './package')
 shutil.rmtree('./package')
