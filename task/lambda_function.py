36c551762b5554ba5ed36fddac848a83238b5f01
Michael Hall <mlh0079@uah.edu>
2021-03-29 13:01:57 -0500

    - Fixed output format for cumulus.

--- task/lambda_function.py
+++ task/lambda_function.py
@@ -1,3 +1,6 @@
+from datetime import time
+from time import mktime, strptime
+
 from main import DiscoverGranules
 
 
@@ -13,5 +16,29 @@ def lambda_handler(event, context=None):
                                           dir_reg_ex=discover_tf['dir_reg_ex'], depth=discover_tf['depth'])
     ret_dict = dg.check_granule_updates(granule_dict)
 
-    event["payload"] = [ret_dict]
+    granules = []
+    for key, value in ret_dict.items():
+        time_str = f"{value['date_modified']} {value['time_modified']} {value['meridiem_modified']}"
+        p = '%m/%d/%Y %I:%M %p'
+        epoch = int(mktime(strptime(time_str, p)))
+        granules.append(
+            {
+                "granule_id": key,
+                "data_type": value["filename"],
+                "version": "1",
+                "files": [
+                    {
+                        "name": value["filename"],
+                        "path": key,
+                        "size": "",
+                        "time": epoch,
+                        "bucket": dg.s3_bucket_name,
+                        "url_path": "",
+                        "type": ""
+                    }
+                ]
+            }
+        )
+
+    event["payload"] = granules
     return event
