9224a6affebd1bb1db6df2e227449332ef3e5920
Michael Hall <mlh0079@uah.edu>
2021-04-05 14:36:29 -0500

    - Implemented code to have properly formatted output for step functions.

--- task/lambda_function.py
+++ task/lambda_function.py
@@ -1,28 +1,37 @@
-from datetime import time
+import os
+import sys
 from time import mktime, strptime
-
 from main import DiscoverGranules
+
+run_cumulus_task = None
+if os.environ.get('CUMULUS_MESSAGE_ADAPTER_DIR'):
+    sys.path.insert(0, os.environ.get('CUMULUS_MESSAGE_ADAPTER_DIR'))
+    from run_cumulus_task import run_cumulus_task
 
 
 def lambda_handler(event, context=None):
     dg = DiscoverGranules()
-    collection = event['meta']['collection']
-    provider = event['meta']['provider']
+    config = event['config']
+    provider = config['provider']
+    collection = event['config']['collection']
     discover_tf = collection['meta']['discover_tf']
 
-    path = f"{provider['protocol']}://{provider['host'].rstrip('/')}/{collection['meta']['provider_path'].lstrip('/')}"
+    # event['config']['collection']['meta']['discover_tf']['output']
+    print(f"discover_tf[{str(discover_tf)}]")
+
+    path = f"{provider['protocol']}://{provider['host'].rstrip('/')}/{config['provider_path'].lstrip('/')}"
+    print(f"path[{path}]")
 
     granule_dict = dg.get_file_links_http(url_path=path, file_reg_ex=discover_tf['file_reg_ex'],
                                           dir_reg_ex=discover_tf['dir_reg_ex'], depth=discover_tf['depth'])
     ret_dict = dg.check_granule_updates(granule_dict)
 
-    granules = []
+    discovered_granules = {}
     for key, value in ret_dict.items():
         time_str = f"{value['date_modified']} {value['time_modified']} {value['meridiem_modified']}"
         p = '%m/%d/%Y %I:%M %p'
         epoch = int(mktime(strptime(time_str, p)))
-        granules.append(
-            {
+        discovered_granules[key] = {
                 "granule_id": value["filename"],
                 "data_type": "",
                 "version": "1",
@@ -38,7 +47,12 @@ def lambda_handler(event, context=None):
                     }
                 ]
             }
-        )
 
-    event["payload"] = granules
-    return event
+    return discovered_granules
+
+
+def handler(event, context):
+    if run_cumulus_task:
+        return run_cumulus_task(lambda_handler, event, context)
+    else:
+        return []
