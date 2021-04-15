e6214f08b8c0b395662275f0704a08803b967fab
amarouane <amarouane@itsc.uah.edu>
2021-04-15 09:12:07 -0500

    Making unique lookup file names to prevent override

--- task/lambda_function.py
+++ task/lambda_function.py
@@ -10,18 +10,17 @@ if os.environ.get('CUMULUS_MESSAGE_ADAPTER_DIR'):
 
 
 def lambda_handler(event, context=None):
-    dg = DiscoverGranules()
+
     config = event['config']
     provider = config['provider']
     collection = event['config']['collection']
     discover_tf = collection['meta']['discover_tf']
-
+    csv_file_name = f"{collection['name']}__{collection['version']}.csv"
+    dg = DiscoverGranules(csv_file_name=csv_file_name)
     path = f"{provider['protocol']}://{provider['host'].rstrip('/')}/{config['provider_path'].lstrip('/')}"
-
     granule_dict = dg.get_file_links_http(url_path=path, file_reg_ex=discover_tf['file_reg_ex'],
                                           dir_reg_ex=discover_tf['dir_reg_ex'], depth=discover_tf['depth'])
     ret_dict = dg.check_granule_updates(granule_dict)
-
     discovered_granules = []
     for key, value in ret_dict.items():
         time_str = f"{value['date_modified']} {value['time_modified']} {value['meridiem_modified']}"
@@ -30,7 +29,6 @@ def lambda_handler(event, context=None):
         host = config['provider']["host"]
         filename = value["filename"]
         path = key[key.find(host) + len(host): key.find(filename)]
-
         discovered_granules.append({
                 "granuleId": value["filename"],
                 "dataType": collection.get("name", ""),
