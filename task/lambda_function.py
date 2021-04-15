a9d1487ce8804334e0619ae1c8370b3f4cd61860
amarouane <amarouane@itsc.uah.edu>
2021-04-15 09:48:54 -0500

    Allow file reg-ex to read from granuleIdExtraction

--- task/lambda_function.py
+++ task/lambda_function.py
@@ -15,10 +15,11 @@ def lambda_handler(event, context=None):
     provider = config['provider']
     collection = event['config']['collection']
     discover_tf = collection['meta']['discover_tf']
+    file_reg_ex = collection.get('granuleIdExtraction')
     csv_file_name = f"{collection['name']}__{collection['version']}.csv"
     dg = DiscoverGranules(csv_file_name=csv_file_name)
     path = f"{provider['protocol']}://{provider['host'].rstrip('/')}/{config['provider_path'].lstrip('/')}"
-    granule_dict = dg.get_file_links_http(url_path=path, file_reg_ex=discover_tf['file_reg_ex'],
+    granule_dict = dg.get_file_links_http(url_path=path, file_reg_ex=file_reg_ex,
                                           dir_reg_ex=discover_tf['dir_reg_ex'], depth=discover_tf['depth'])
     ret_dict = dg.check_granule_updates(granule_dict)
     discovered_granules = []
