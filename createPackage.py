32ac5a9219ebed8121628cfaf97ce3c797216e9d
Michael Hall <mlh0079@uah.edu>
2021-09-10 16:50:44 -0500

    - Added seperate requirements.txt for lambda.

--- createPackage.py
+++ createPackage.py
@@ -1,7 +1,7 @@
 import shutil
 import pip
 
-pip.main(['install', '--target', './package', '-r', 'requirements.txt'])
+pip.main(['install', '--target', './package', '-r', 'requirements-lambda.txt'])
 [shutil.copy(ele, './package/') for ele in ['./task/lambda_function.py', './task/main.py']]
 shutil.make_archive('./package', 'zip', './package')
 shutil.rmtree('./package')
