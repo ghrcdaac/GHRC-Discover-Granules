f6c2c8f5f44c343652f166f384910dd08e8aa81c
Michael Hall <mlh0079@uah.edu>
2022-06-06 14:59:09 -0500

    - Removed commented code.

--- createPackage.py
+++ createPackage.py
@@ -9,9 +9,5 @@ for ele in os.listdir(task_dir):
     if ele.endswith('.py'):
         shutil.copy(f'./task/{ele}', './package/task')
 
-# libs_dir = f'{os.getcwd()}/libs'
-# for ele in os.listdir(libs_dir):
-#     shutil.copytree(f'./libs/{ele}', './package', dirs_exist_ok=True)
-
 shutil.make_archive('./package', 'zip', './package')
 shutil.rmtree('./package')
