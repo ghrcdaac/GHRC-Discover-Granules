ec6b4ca3938ce838a538daaf8c41aded04ffd725
Michael Hall <mlh0079@uah.edu>
2022-08-01 16:06:03 -0500

    - Added batching for cleanup script.

--- dev_utils/ingest_cleanup.py
+++ dev_utils/ingest_cleanup.py
@@ -1,4 +1,7 @@
 import concurrent.futures
+import os
+from time import sleep
+
 import boto3
 
 
@@ -6,15 +9,17 @@ import boto3
 This script is only intended to be used by developers as a way to quickly cleanup ingest workflow executions. The ARN
 is hardcoded but should only ever have to be updated if the SBX stack gets redeployed. 
 """
-boto3.setup_default_session(profile_name='<ADD_PROFILE_HERE>')
 
 
-def main():
+def cleanup():
+    boto3.setup_default_session(profile_name=os.getenv('AWS_PROFILE'), region_name=os.getenv('AWS_REGION'))
     client = boto3.client('stepfunctions')
 
     tasks_to_kill = []
+    tasks_to_kill_len = len(tasks_to_kill)
     args = {
-        'stateMachineArn': 'arn:aws:states:us-west-2:<ADD_ACCOUNT_NUMBER_HERE>:stateMachine:<ADD_STACK_PREFIX_HERE>-IngestGranule',
+        'stateMachineArn': f'arn:aws:states:us-west-2:{os.getenv("ACCOUNT_NUMBER")}'
+                           f':stateMachine:{os.getenv("STACK_PREFIX")}-IngestGranule',
         'statusFilter': 'RUNNING'
     }
 
@@ -23,7 +28,7 @@ def main():
             resp = client.list_executions(**args)
             tasks_to_kill += resp.get("executions")
             next_token = resp.get('nextToken')
-            if next_token:
+            if next_token and not len(tasks_to_kill) % 100:
                 args.setdefault('nextToken', next_token)
             else:
                 break
@@ -31,6 +36,7 @@ def main():
         with concurrent.futures.ThreadPoolExecutor() as executor:
             futures = []
             for e in resp.get('executions'):
+                print(f'Submitting for termination: {e.get("executionArn")}')
                 futures.append(
                     executor.submit(client.stop_execution, executionArn=e.get('executionArn'))
                 )
@@ -39,10 +45,13 @@ def main():
                 error = future.exception()
                 if error:
                     print(f'Error: {error}')
-                else:
-                    print(future.result())
 
-        print(f'Killed {len(tasks_to_kill)} tasks.')
+        if len(tasks_to_kill) != tasks_to_kill_len:
+            tasks_to_kill_len = len(tasks_to_kill)
+            print(f'Killed {len(tasks_to_kill)} tasks.')
+        else:
+            print('waiting for tasks...')
+            sleep(5)
 
 
 def fast_copy():
@@ -68,6 +77,5 @@ def fast_copy():
         # Rename file
 
 
-
 if __name__ == '__main__':
-    main()
+    pass
