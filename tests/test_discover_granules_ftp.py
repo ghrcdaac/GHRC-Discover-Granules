cf988e446825f74745d767d74cdebe28994511c4
Michael Hall <mlh0079@uah.edu>
2023-05-03 16:24:19 -0500

    - Initial unit tests for ftp.

---
+++ tests/test_discover_granules_ftp.py
@@ -0,0 +1,21 @@
+import re
+import unittest
+from unittest.mock import patch, MagicMock
+
+from task.discover_granules_ftp import DiscoverGranulesFTP
+
+
+class TestDiscoverGranules(unittest.TestCase):
+    def setUp(self) -> None:
+        event = MagicMock()
+        self.dg_client = DiscoverGranulesFTP(event)
+
+    @patch.object(re, 'search')
+    def test_process_ftp_list_output(self, re_mock):
+        with open('ftp_list_output.txt', 'r+', encoding='utf-8') as sample_output:
+            self.output = sample_output.read()
+        self.dg_client.dbm.add_record = MagicMock()
+        directory_list = []
+        self.dg_client.process_ftp_list_output(self.output, directory_list)
+        self.assertEqual(3, self.dg_client.dbm.add_record.call_count)
+        self.assertEqual(3, len(directory_list))
