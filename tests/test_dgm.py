f444c7b7c75825f68a25c0d23f077827739880cb
Michael Hall <mlh0079@uah.edu>
2023-05-05 15:31:58 -0500

    - Small cleanup

--- tests/test_dgm.py
+++ tests/test_dgm.py
@@ -1,11 +1,11 @@
 
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock
 
 from task.dbm_base import DBManagerBase
 from task.dgm import get_db_manager
 from task.dgm_cumulus import DBManagerCumulus
-from task.dgm_postgresql import DBManagerPostgresql, DB
+from task.dgm_postgresql import DBManagerPostgresql
 from task.dgm_sqlite import DBManagerSQLite
 
 
@@ -29,21 +29,17 @@ class TestDGM(unittest.TestCase):
 
             def close_db(self):
                 super().close_db()
-                pass
 
             def add_record(self, name=None, granule_id=None, collection_id=None, etag=None, last_modified=None,
                            size=None):
                 super().add_record(name=name, granule_id=granule_id, collection_id=collection_id, etag=etag,
                                    last_modified=last_modified, size=size)
-                pass
 
             def flush_dict(self):
                 super().flush_dict()
-                pass
 
             def read_batch(self, collection_id=None, provider_path=None, batch_size=None):
                 super().read_batch(collection_id=collection_id, provider_path=provider_path, batch_size=batch_size)
-                pass
 
         test_dbm = TestDBM()
         self.assertRaises(NotImplementedError, test_dbm.close_db)
