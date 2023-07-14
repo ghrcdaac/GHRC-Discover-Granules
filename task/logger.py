99d8a05ae7407a565b0e75c6ceacef4d726deeed
Michael Hall <mlh0079@uah.edu>
2023-07-14 14:10:26 -0500

    - All cumulus logger functions are now handled by the local logger class.

--- task/logger.py
+++ task/logger.py
@@ -3,18 +3,21 @@ import os
 from cumulus_logger import CumulusLogger
 
 
-class MyLogger:
+class LocalLogger:
+    """
+    Class used for logging if the CumulusLogger is not available
+    """
+    log = print
+    trace = print
+    debug = print
     info = print
+    warn = print
     warning = print
     error = print
-    """
-    Class used for logging
-    """
-    pass
 
 
 gdg_logger = CumulusLogger(name='GHRC-Discover-Granules', level=logging.INFO) \
-    if os.getenv('enable_logging', 'false').lower() == 'true' else MyLogger()
+    if os.getenv('enable_logging', 'false').lower() == 'true' else LocalLogger()
 
 if __name__ == '__main__':
     pass
