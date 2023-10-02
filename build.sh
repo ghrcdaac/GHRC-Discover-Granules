4e36bcd2e35da8000f5b8a168f6269dde68688a6
Michael Hall <mlh0079@uah.edu>
2023-10-02 15:14:14 -0500

    - Removing profile check for now.

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ docker build . -t discover-granules &&
   docker cp "${CID}":/var/task/package/package.zip ./package.zip &&
   docker rm "${CID}" &&
 
-  if [[ -z ${WSBX+x} ]]; then
-    aws lambda update-function-code --profile WSBX --region=us-west-2 --function-name arn:aws:lambda:"${AWS_REGION}":"${AWS_ACCOUNT_NUMBER}":function:"${PREFIX}"-ghrc-discover-granules --zip-file fileb://package.zip --publish
-  fi
+#  if [[ -z ${WSBX+x} ]]; then
+#    aws lambda update-function-code --profile WSBX --region=us-west-2 --function-name arn:aws:lambda:"${AWS_REGION}":"${AWS_ACCOUNT_NUMBER}":function:"${PREFIX}"-ghrc-discover-granules --zip-file fileb://package.zip --publish
+#  fi
