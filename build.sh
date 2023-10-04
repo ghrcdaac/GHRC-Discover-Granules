bf4bcf22c4f513bbbca315acf3286915b79f422f
Michael Hall <mlh0079@uah.edu>
2023-10-04 09:32:34 -0500

    - Fixing build script for CICD process.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 docker build . -t discover-granules &&
   CID=$(docker create discover-granules) &&
   docker cp "${CID}":/var/task/package/package.zip ./package.zip &&
-  docker rm "${CID}" &&
+  docker rm "${CID}"
 
 #  if [[ -z ${WSBX+x} ]]; then
 #    aws lambda update-function-code --profile WSBX --region=us-west-2 --function-name arn:aws:lambda:"${AWS_REGION}":"${AWS_ACCOUNT_NUMBER}":function:"${PREFIX}"-ghrc-discover-granules --zip-file fileb://package.zip --publish
