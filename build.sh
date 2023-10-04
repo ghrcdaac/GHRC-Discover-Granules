c4305c5ff7cd9879bdacad2ec5940725baacd0f1
Michael Hall <mlh0079@uah.edu>
2023-10-04 10:25:04 -0500

    - build script should no longer error in CICD.

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 docker build . -t discover-granules &&
   CID=$(docker create discover-granules) &&
   docker cp "${CID}":/var/task/package/package.zip ./package.zip &&
-  docker rm "${CID}"
-
-#  if [[ -z ${WSBX+x} ]]; then
-#    aws lambda update-function-code --profile WSBX --region=us-west-2 --function-name arn:aws:lambda:"${AWS_REGION}":"${AWS_ACCOUNT_NUMBER}":function:"${PREFIX}"-ghrc-discover-granules --zip-file fileb://package.zip --publish
-#  fi
+  docker rm "${CID}" &&
+  if [[ -n ${AWS_PROFILE} ]]; then
+    aws lambda update-function-code --profile "${AWS_PROFILE}" --region=us-west-2 --function-name arn:aws:lambda:"${AWS_REGION}":"${AWS_ACCOUNT_NUMBER}":function:"${PREFIX}"-ghrc-discover-granules --zip-file fileb://package.zip --publish
+  fi
