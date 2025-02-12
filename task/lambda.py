8afc8448b1718bb832f16e366ade9a45dff98300
Michael Hall <mlh0079@uah.edu>
2025-02-12 10:43:10 -0600

    - Reorganizing the code to make the entrypoints more clear for lambda vs ecs

---
+++ task/lambda.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+from task.test import test_main
+from task.logger import gdg_logger
+from task.main import main
+
+if os.environ.get('CUMULUS_MESSAGE_ADAPTER_DIR'):
+    sys.path.insert(0, os.environ.get('CUMULUS_MESSAGE_ADAPTER_DIR'))
+    from run_cumulus_task import run_cumulus_task
+
+
+def handler(event, context):
+    # gdg_logger.info(f'Full Event: {event}')
+    if event.get('is_test', False):
+        results = test_main(event, context)
+    else:
+        if run_cumulus_task:
+            results = run_cumulus_task(main, event, context)
+            # gdg_logger.info(f'result: {results}')
+        else:
+            results = main(event, context)
+    return results
